aa3806a1cf65e1598d3b01f0c43036d303cea936
elyasaf1230 <elyasafxboxlive@gmail.com>
2017-03-16 19:44:15 +0200

    Protected Array is finished!

--- C++/2 - protected array/Main.cpp
+++ C++/2 - protected array/Main.cpp
@@ -1,16 +1,54 @@
 #include <iostream>
+#include <exception>
+#include <string>
+#include "ProtectedArray.h"
 
 
 int main()
-{
-	int arr[] = { 1,2,3,4,5,6,7,8,9 };
-	int* brr = arr;
-	++brr;
+{	
+	ProtectedArray<int> arr(10);
+	
+	std::cout << "before:" << std::endl;
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout << i << " -> " << (arr[i] = 1) << std::endl;
+	}
 
-	std::cout << arr[0] << std::endl;
-	std::cout << brr[0] << std::endl;
-	std::cout << brr[-1] << std::endl;
-	std::cout << arr[-1] << std::endl;
+	ProtectedArray<int> brr = arr;
+	
+	//arr[0]
+	brr[0] = 0;
+	
+	//arr[1]
+	++brr;
+	(*brr) = 0;
+	
+	//arr[3]
+	ProtectedArray<int> crr = brr + 2;
+	(*crr) = 0;
+
+	//invalid index..
+	try
+	{
+		crr += 10;
+	}
+	catch (std::exception e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
+	//You can't do: int arr[]; int brr[]; brr = arr;
+	//But hey! it's c++, so with my ProtectedArray you can! 
+	crr = arr + 7;
+
+	//arr[8]
+	crr[1] = 0;
+	
+	std::cout << "after:" << std::endl;
+	for (int i = 0; i < 10; ++i)
+	{
+		std::cout << i << " -> " << (arr[i]) << std::endl;
+	}
 
 	getchar();
 	return 1;
--- C++/2 - protected array/ProtectedArray.h
+++ C++/2 - protected array/ProtectedArray.h
@@ -4,7 +4,10 @@
 #include <exception>
 #include <string>
 
-template <class T> 
+const int ioor_i = 1;
+const std::string ioor_s = "Index out of range!";
+
+template <class T>
 class ProtectedArray
 {
 private:
@@ -14,10 +17,14 @@ private:
 	int _size;		//size of the array.
 	int _index;		//first element in array (for operator++)
 
+	int* _count;		//this will idicates how much objects have the same array.
+
+	ProtectedArray<T>& precedeArray(const int offset);
+
 public:
 	//constructors:
 	ProtectedArray(int size);
-	ProtectedArray(ProtectedArray<T> &other); 
+	ProtectedArray(ProtectedArray<T> &other);
 
 	//destructor:
 	~ProtectedArray();
@@ -31,4 +38,272 @@ public:
 	T& operator*();
 	ProtectedArray<T>& operator++();
 	ProtectedArray<T>& operator--();
+	ProtectedArray<T> operator+(int offset);
+	ProtectedArray<T> operator-(int offset);
+	ProtectedArray<T>& operator+=(int offset);
+	ProtectedArray<T>& operator-=(int offset);
 };
+
+/*
+	Constructor.
+
+	INPUT:
+		size - the size of the array to allocate.
+
+	OUTPUT:
+		None.
+*/
+template <typename T>
+ProtectedArray<T>::ProtectedArray(int size)
+{
+	this->_array = new T[size];
+	this->_capacity = size;
+	this->_size = size;
+	this->_index = 0;
+
+	this->_count = new(int);
+	*this->_count = 1;
+}
+
+/*
+	Copy constructor.
+
+	INPUT:
+		other - ProtectedArray to copy.
+
+	OUTPUT:
+		None.
+*/
+template <typename T>
+ProtectedArray<T>::ProtectedArray(ProtectedArray<T> &other)
+{
+	//We won't 'deep' copy the array because we want it to behave just like a normal array.
+	this->_array = other._array;
+
+	this->_capacity = other._capacity;
+	this->_index = other._index;
+	this->_size = other._size;
+
+	this->_count = other._count;
+	*this->_count += 1;
+}
+
+/*
+	Destructor:
+
+	INPUT:
+		None.
+
+	OUTPUT:
+		None.
+*/
+template <typename T>
+ProtectedArray<T>::~ProtectedArray()
+{
+	*this->_count -= 1;
+
+	if (*this->_count == 0) //is this the last object the this array??? 
+	{
+		delete[] this->_array;
+		delete this->_count;
+	}
+}
+
+/*
+	getCapacity:
+		This method return the aloocated size of the array.
+
+	INPUT:
+		None.
+
+	OUTPUT:
+		int - the allocated size of the array.
+*/
+template <typename T>
+int ProtectedArray<T>::getCapacity() const
+{
+	return this->_capacity;
+}
+
+/*
+	getSize:
+		This method return the size of the array.
+
+	INPUT:
+		None.
+
+	OUTPUT:
+		int - the size.
+*/
+template <typename T>
+int ProtectedArray<T>::getSize() const
+{
+	return this->_size;
+}
+
+/*
+	operator[]:
+
+	INPUT:
+		index - index of element from the array to return.
+
+	OUTPUT:
+		T - reference to the element.
+		will throw exception of "index out of range" if index is not in the range of the array.
+*/
+template <typename T>
+T& ProtectedArray<T>::operator[](const int index)
+{
+	//   for possitive index        for negative index
+	if (index > this->_size || index < this->_index * -1) //not in range
+	{
+		throw std::exception(ioor_s.c_str(), ioor_i);
+	}
+
+	return (this->_array + this->_index)[index];
+}
+
+/*
+	operator*:
+
+	INPUT:
+		None.
+
+	OUTPUT:
+		T - referance to the first element in the array.
+*/
+template <typename T>
+T& ProtectedArray<T>::operator*()
+{
+	return (this->_array + this->_index)[0];
+}
+
+/*
+	operator++:
+
+	INPUT:
+		None.
+
+	OUTPUT:
+		Referance to the current object (array is + 1).
+		Will throw exception of "index out of range" if reach to the last element and try to go after it.
+*/
+template <typename T>
+ProtectedArray<T>& ProtectedArray<T>::operator++()
+{
+	return this->precedeArray(1);
+}
+
+/*
+	operator--:
+
+	INPUT:
+		None.
+
+	OUTPUT:
+		Referance to the current object (array is -1).
+		Will throw exception of "index out of range" if reach to the first element and try to go before it.
+*/
+template <typename T>
+ProtectedArray<T>& ProtectedArray<T>::operator--()
+{
+	return this->precedeArray(-1);
+}
+
+/*
+	operator +:
+
+	INPUT:
+		offset - number to move on the array (like operator ++, but more advanced :P)
+
+	OUTPUT:
+		New object.
+		Will throw exception of "index out of range" if will go out of range... da...
+*/
+template <typename T>
+ProtectedArray<T> ProtectedArray<T>::operator+(int offset)
+{
+	ProtectedArray<T> ret(*this);
+
+	ret.precedeArray(offset);
+
+	return ret;
+}
+
+/*
+	operator -:
+
+	INPUT:
+		offset - number to move on the array (like operator --, but more advanced :P)
+
+	OUTPUT:
+		New object.
+		Will throw exception of "index out of range" if will go out of range... da...
+*/
+template <typename T>
+ProtectedArray<T> ProtectedArray<T>::operator-(int offset)
+{
+	ProtectedArray ret(*this);
+
+	ret.precedeArray(-offset);
+
+	return ret;
+}
+
+/*
+	operator +=:
+
+	INPUT:
+		offset - number to move on the array.
+
+	OUTPUT:
+		referance to the current object.
+		Will throw exceprion of "index out of range" if out of the range of the array.
+*/
+template <typename T>
+ProtectedArray<T>& ProtectedArray<T>::operator+=(int offset)
+{
+	return this->precedeArray(offset);
+}
+
+/*
+	operator -=:
+
+	INPUT:
+		offset - number to move on the array.
+
+	OUTPUT:
+		referance to the current object.
+		Will throw exceprion of "index out of range" if out of the range of the array.
+*/
+template <typename T>
+ProtectedArray<T>& ProtectedArray<T>::operator-=(int offset)
+{
+	return this->precedeArray(-offset);
+}
+
+/*
+	This is a private method for the class use.
+	It moving the array first pointer by 'offset' steps.
+
+	INPUT:
+		offset - steps to move the array pointer.
+
+	OUTPUT:
+		Referance to the current object.
+		This method will throw "index out of range" exception if the offset is too high of low.
+*/
+template <typename T>
+ProtectedArray<T>& ProtectedArray<T>::precedeArray(const int offset)
+{
+	//if offset is positive - size will decrease (and need to check if size will be greater then 0).
+	//if offset is negative - size will increase (and need to check is size will be less then capacity).
+	if ((offset >= 0 && this->_size - offset < 0) ||
+		(offset < 0 && this->_size + (-offset) > this->_capacity))
+			throw std::exception(ioor_s.c_str(), ioor_i);
+
+	this->_index += offset;
+	this->_size -= offset;
+
+	return *this;
+}
